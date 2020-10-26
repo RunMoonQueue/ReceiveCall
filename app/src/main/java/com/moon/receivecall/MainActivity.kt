0c4e8b1acf13473eaa6387a9251b8d45a0d40644
LAPTOP-6LQBHRCU\skyli <skylife927@naver.com>
2020-10-26 23:17:22 +0900

    Create MessageReceiver.kt

--- app/src/main/java/com/moon/receivecall/MainActivity.kt
+++ app/src/main/java/com/moon/receivecall/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.moon.receivecall
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        processIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        processIntent(intent)
+        super.onNewIntent(intent)
+    }
+
+    private fun processIntent(intent: Intent?) {
+        intent?.run{
+            val sender = getStringExtra("sender")
+            val content = getStringExtra("contents")
+
+        }
     }
 }
---
+++ app/src/main/java/com/moon/receivecall/MessageReceiver.kt
@@ -0,0 +1,52 @@
+package com.moon.receivecall
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.telephony.SmsMessage
+import android.util.Log
+
+
+class MessageReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val bundle = intent?.extras
+        val messages = parseMessage(bundle!!)
+        if (messages!!.isNotEmpty()) {
+            val sender = messages[0]?.originatingAddress
+            Log.d(TAG, "sender: $sender")
+
+            val contents = messages[0]?.messageBody.toString()
+            Log.d(TAG, "contents:$contents")
+            Log.d(TAG, "context:$context")
+            context?.startActivity(Intent(context, MainActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                putExtra("sender", sender)
+                putExtra("contents", contents)
+            })
+        }
+
+    }
+
+    private fun parseMessage(bundle: Bundle): Array<SmsMessage?>? {
+        val objs = bundle["pdus"] as Array<Any>?
+        val messages = arrayOfNulls<SmsMessage>(
+            objs!!.size
+        )
+        for (i in objs.indices) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val format = bundle.getString("format")
+                messages[i] = SmsMessage.createFromPdu(objs[i] as ByteArray, format)
+            } else {
+                messages[i] = SmsMessage.createFromPdu(objs[i] as ByteArray)
+            }
+        }
+        return messages
+    }
+
+    companion object {
+        const val TAG = "MessageReceiver"
+    }
+}
